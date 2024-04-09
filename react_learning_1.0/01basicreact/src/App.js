f69405dbc60e0c75758c1cc179f5fd58c3747c00
aayushdkl <079bei001.aayush@sagarmatha.edu.np>
2024-04-09 17:04:13 +0545

    first commit

--- react_learning_1.0/01basicreact/src/App.js
+++ react_learning_1.0/01basicreact/src/App.js
@@ -1,7 +1,7 @@
 
 function App() {
   return (
-    <h1>learning react</h1>
+    <h1>learning react | My name is Aayush Dhakal and in this video i understood how package.json is the main file and in which all the things are available to look at. I removed all the non useful and non understandable code files, and learnt about bundler like vite and how to initiate the react project</h1>
   );
 }
 
