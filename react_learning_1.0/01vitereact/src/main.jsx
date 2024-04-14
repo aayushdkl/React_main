05c64861a7ea3cf959901707ef3af8acecf85cc1
aayushdkl <079bei001.aayush@sagarmatha.edu.np>
2024-04-14 15:15:17 +0545

    learning JSX

--- react_learning_1.0/01vitereact/src/main.jsx
+++ react_learning_1.0/01vitereact/src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank',
+//   },
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+//   children: 'Click me to visit Google!',
+// }
+
+const reactElement = (
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
 )
+ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+  //  anotherElement
+  reactElement
+
+)  
+                      
+        
--- react_learning_2.0/src/App.jsx
+++ react_learning_2.0/src/App.jsx
@@ -1,6 +1,17 @@
-
 function App() {
-  return "Hello World!"
+  return (
+    <div className="large" id="largeDiv">
+      <label htmlFor="InputId">There goes the alarm</label>
+      <input id="InputId" type="number" defaultValue={3}></input>
+
+      <div>My value is : {null}</div>
+      <div>My value is : {undefined}</div>
+      <div>My value is : {false}</div>
+
+      <div>My value is : {true}</div>
+      <div>My value is : {0}</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
